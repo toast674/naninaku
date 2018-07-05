bfb890f3d4ebe0cd3616417f7728a1ba2677299f
hiromasa <toast674@gmail.com>
2018-07-05 15:52:18 +0900

    Initial Commit

---
+++ naninaku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  naninaku
+//
+//  Created by 苅宿大將 on 2018/07/05.
+//  Copyright © 2018年 mizuno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
